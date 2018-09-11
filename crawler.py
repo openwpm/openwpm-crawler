0e5dec3816be01fe496eeb849b6280167c945ccd
englehardt <englehardt@gmail.com>
2018-09-11 18:36:25 +0000

    Update crawler + add sample analysis notebook

--- crawler.py
+++ crawler.py
@@ -8,8 +8,9 @@ import crawl_utils as cu
 from OpenWPM.automation import CommandSequence, TaskManager
 
 NUM_BROWSERS = 15
+OUT_DIR = '2018-09-11_top_50k_stateless'
 SITES = ['http://' + x for x in cu.get_top_1m(
-    os.path.expanduser('~/Desktop/'))]
+    os.path.expanduser('~/Desktop/%s/' % OUT_DIR))]
 
 manager_params, browser_params = TaskManager.load_default_params(NUM_BROWSERS)
 
@@ -19,14 +20,14 @@ for i in range(NUM_BROWSERS):
     browser_params[i]['http_instrument'] = True
     browser_params[i]['headless'] = True
 
-manager_params['data_directory'] = '~/Desktop/s3-test/'
-manager_params['log_directory'] = '~/Desktop/s3-test/'
+manager_params['data_directory'] = '~/Desktop/%s/' % OUT_DIR
+manager_params['log_directory'] = '~/Desktop/%s/' % OUT_DIR
 manager_params['output_format'] = 's3'
 manager_params['s3_bucket'] = 'openwpm-crawls'
-manager_params['s3_directory'] = '2018-09-11_top_10k_stateless-batches'
+manager_params['s3_directory'] = OUT_DIR 
 
 manager = TaskManager.TaskManager(manager_params, browser_params)
-for site in SITES[0:1000]:
+for site in SITES[0:50000]:
     command_sequence = CommandSequence.CommandSequence(site, reset=True)
     command_sequence.get(sleep=10, timeout=60)
     manager.execute_command_sequence(command_sequence)
