f9e42fb33b17357ba4e14cee401a37dcffc4d1cd
englehardt <englehardt@gmail.com>
2018-09-26 16:54:14 -0400

    Update crawl file for new crawl

--- crawler.py
+++ crawler.py
@@ -23,10 +23,10 @@ manager_params['data_directory'] = '~/Desktop/s3-test/'
 manager_params['log_directory'] = '~/Desktop/s3-test/'
 manager_params['output_format'] = 's3'
 manager_params['s3_bucket'] = 'openwpm-crawls'
-manager_params['s3_directory'] = '2018-09-11_top_10k_stateless-batches'
+manager_params['s3_directory'] = '2018-09-26_top_10k_stateless-partitioned'
 
 manager = TaskManager.TaskManager(manager_params, browser_params)
-for site in SITES[0:1000]:
+for site in SITES[0:10000]:
     command_sequence = CommandSequence.CommandSequence(site, reset=True)
     command_sequence.get(sleep=10, timeout=60)
     manager.execute_command_sequence(command_sequence)
