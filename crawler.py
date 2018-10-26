f588defd9fc747a267bbdfa027f347ce8d77d647
englehardt <englehardt@gmail.com>
2018-10-26 09:54:27 -0700

    Update crawl configuration

--- crawler.py
+++ crawler.py
@@ -8,7 +8,7 @@ import crawl_utils as cu
 from OpenWPM.automation import CommandSequence, TaskManager
 
 NUM_BROWSERS = 15
-OUT_DIR = '2018-09-26_top_20k_stateless'
+OUT_DIR = '2018-10-26_top_10k_stateless_js-content'
 SITES = ['http://' + x for x in cu.get_top_1m(
     os.path.expanduser('~/Desktop/%s/' % OUT_DIR))]
 
@@ -17,6 +17,7 @@ manager_params, browser_params = TaskManager.load_default_params(NUM_BROWSERS)
 for i in range(NUM_BROWSERS):
     browser_params[i]['cookie_instrument'] = True
     browser_params[i]['js_instrument'] = True
+    browser_params[i]['save_javascript'] = True
     browser_params[i]['http_instrument'] = True
     browser_params[i]['headless'] = True
 
@@ -27,7 +28,7 @@ manager_params['s3_bucket'] = 'openwpm-crawls'
 manager_params['s3_directory'] = OUT_DIR
 
 manager = TaskManager.TaskManager(manager_params, browser_params)
-for site in SITES[0:20000]:
+for site in SITES[0:10000]:
     command_sequence = CommandSequence.CommandSequence(site, reset=True)
     command_sequence.get(sleep=10, timeout=60)
     manager.execute_command_sequence(command_sequence)
