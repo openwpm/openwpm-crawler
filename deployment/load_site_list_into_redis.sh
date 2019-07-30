7a3bfa40ca9859457caa48796870be0d48c3eab8
Fredrik Wollsén <fredrik@neam.se>
2019-07-30 16:53:18 +0300

    Support loading sites with quotes into redis

--- deployment/load_site_list_into_redis.sh
+++ deployment/load_site_list_into_redis.sh
@@ -14,8 +14,11 @@ echo -e "\nEnqueuing site list in redis"
 echo "DEL $REDIS_QUEUE_NAME" > joblist.txt
 echo "DEL $REDIS_QUEUE_NAME:processing" >> joblist.txt
 
-# Add site list in reverse order since the queue gets worked upon from the bottom up
-cat "$SITE_LIST_CSV" | sed '1!G;h;$!d' | sed "s/^/RPUSH $REDIS_QUEUE_NAME /" >> joblist.txt
+# sed #1 = Add site list in reverse order since the queue gets worked upon from the bottom up
+# sed #2 = Quote single quotes
+# awk #1 = Add the RPUSH command with the site value within single quotes
+cat "$SITE_LIST_CSV" | sed '1!G;h;$!d' | sed "s/'/\\\'/g" | awk -F ',' 'FNR > 0 {print "RPUSH '$REDIS_QUEUE_NAME' '\''"$1","$2"'\''"}' >> joblist.txt
+
 kubectl cp joblist.txt redis-master:/tmp/joblist.txt
 kubectl exec redis-master -- sh -c "cat /tmp/joblist.txt | redis-cli --pipe"
 
