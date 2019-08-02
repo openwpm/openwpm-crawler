3f620819ce09df741162ea8f162d87b883c0a344
englehardt <englehardt@gmail.com>
2019-08-02 09:46:50 -0700

    Reorder macos install in the hopes that it helps plyvel issues

--- setup-python-venv-mac.sh
+++ setup-python-venv-mac.sh
@@ -6,14 +6,17 @@ python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
 
-# Install requirements
-pip install -U -r requirements.txt
+# One of the requirements (openwpm-utils) requires plyvel, which doesn't
+# install correctly from pip on MacOS. Let's install it here first.
 
 # A recent version of leveldb is required
 brew install leveldb || brew upgrade leveldb
 
 # Make sure we build plyvel properly to work with the installed leveldb on recent OSX versions
 CFLAGS='-mmacosx-version-min=10.7 -stdlib=libc++ -std=c++11' pip install --force-reinstall --ignore-installed --no-binary :all: plyvel
+
+# Install requirements
+pip install -U -r requirements.txt
 
 # Update the corresponding Jupyter kernel
 ipython kernel install --user --name=openwpm-crawler
